506c6600343f0cdce6dc2c675c97f74431b7901e
Matias Pompilio <matias.pompilio@nan-labs.com>
2022-10-30 18:27:37 -0300

    removing comments

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-/* eslint-disable */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-/* eslint-enable */
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
