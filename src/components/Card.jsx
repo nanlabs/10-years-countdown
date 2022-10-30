c59ffffd0faa77e9066adba4374f64d6121a7410
Matias Pompilio <matias.pompilio@nan-labs.com>
2022-10-30 18:31:13 -0300

    test build

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,15 +16,6 @@ const StyledImg = styled.img`
   filter: ${(props) => (props.disabled ? 'grayscale(1)' : 'none')};
 `;
 
-/* const GridItem = styled.div`
-  grid-column: span 2;
-
-  &:nth-child(5n),
-  &:nth-child(5n-1) {
-    grid-column: span 3;
-  };
-`; */
-
 function Card({
   img,
   name,
