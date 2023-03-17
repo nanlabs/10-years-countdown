2ee189d73ca6ab7c37fb8ecb94a53c8141c913a8
Nabor Torres <arsentau@gmail.com>
2023-03-17 16:07:42 -0300

    Draft

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,11 @@
+// Create a new component called Spotify that returns allow me to embed a Spotify playlist
+
+function Spotify() {
+  return (
+    <div>
+      {/* <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1j6flBxNJC54Jb7AafwuGD?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe> */}
+    </div>
+  );
+}
+
+export default Spotify;
