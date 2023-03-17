f8fd152a32979a4594a86769941868c93ce97482
Nabor Torres <arsentau@gmail.com>
2023-03-17 16:50:41 -0300

    Add draft idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import { useState, useMemo } from 'react';
 import ReactConfetti from 'react-confetti';
 import styled from 'styled-components';
-import Album from './components/Album';
-import MusicPlayer from './components/MusicPlayer';
 import SpotifyCustomPlayer from './components/Spotify';
 import { ConfettiContext } from './context/confetti';
+import bgImg from './assets/bg.svg';
+import anniversaryImg from './assets/anniversary.svg';
 
 const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   position: relative;
   gap: 50px;
+  height: 100vh;
+  width: 100vw;
+  background: url(${bgImg}) no-repeat center center fixed;
+  background-size: cover;
+`;
+
+const CentralCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  position: relative;
+  padding: 2em;
+  background: #ffffff50;
+  border-radius: 10px;
+  box-shadow: 0 0 1em rgba(0, 0, 0, 0.2);
+  width: 800px;
+  backdrop-filter: blur(10px);
+`;
+
+const StyledImg = styled.img`
+  width: 200px;
 `;
 
 function App() {
@@ -25,10 +47,11 @@ function App() {
   return (
     <ConfettiContext.Provider value={contextValue}>
       <AppContainer>
-        { confettiActive && <ReactConfetti /> }
-        <Album />
-        <MusicPlayer />
-        <SpotifyCustomPlayer />
+        <CentralCard>
+          { confettiActive && <ReactConfetti /> }
+          <StyledImg src={anniversaryImg} alt="anniversary" />
+          <SpotifyCustomPlayer />
+        </CentralCard>
       </AppContainer>
     </ConfettiContext.Provider>
   );
